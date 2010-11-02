584f417035857b8b1196c58d8eb376d5cec6d611
Anuj Luthra <anuj.luthra@gmail.com>
2010-11-02 11:23:38 +1030

    simple client to send out IM

---
+++ google_chat.rb
@@ -0,0 +1,26 @@
+require 'xmpp4r/client'
+include Jabber
+
+class GoogleChat
+  include Jabber
+  
+
+  def initialize(user, password, resource='Resource')
+    jid = JID::new("#{user}/#{resource}")
+    @client = Client::new(jid)
+    @client.use_ssl = true
+    begin
+      @client.connect('talk.google.com', 443)
+      @client.auth(password)
+      return @self
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+
+  def send(message,to)
+    @client.send  Message::new(to, message).set_type(:normal).set_id('1')
+  end
+
+end
